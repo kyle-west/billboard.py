a248a2789a8be425459e2753768d62af54e6dce7
Kyle West <kyle-west@users.noreply.github.com>
2023-05-10 14:35:00 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 # DEPENDENCIES:
 # https://github.com/pwaller/pyfiglet
 
-if [ `command -v pip` != "" ]; then
-  pip install pyfiglet
-else
-  python -m pip install pyfiglet
-fi
+python -m pip install pyfiglet
